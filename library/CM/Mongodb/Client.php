9e9274fb59c4f80a6ad5af3f1da44fe2a3fd6799
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-03-06 13:55:05 +0100

    base MongoDB client

---
+++ library/CM/Mongodb/Client.php
@@ -0,0 +1,43 @@
+<?php
+
+class CM_Mongodb_Client extends CM_Class_Abstract {
+
+  /** @var \Memcache */
+  private $_mongodb = null;
+  private $_db = null;
+
+  public function __construct() {
+    $this->_mongodb = new MongoClient();
+    $this->useDatabase('fuckbook');
+  }
+
+  public function useDatabase($databaseName) {
+    if ($this->_db) {
+      unset($this->_db);
+    }
+    $this->_db = $this->_mongodb->{$databaseName};
+  }
+
+  public function insert($collection, $object) {
+    $this->_db->{$collection}->insert($object);
+  }
+
+  public function findOne($collection, $query) {
+    return $this->_db->{$collection}->findOne();
+  }
+
+  public function drop($collection) {
+    $this->_db->drop($collection);
+  }
+
+  /**
+   * @return CM_Mongodb_Client
+   */
+  public static function getInstance() {
+    static $instance;
+    if (!$instance) {
+      $instance = new self();
+    }
+    return $instance;
+  }
+}
